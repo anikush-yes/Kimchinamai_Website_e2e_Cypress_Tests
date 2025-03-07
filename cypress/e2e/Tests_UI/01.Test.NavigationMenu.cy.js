c7d7cae1ced8668e453c700be93d6e11098ad779
anikush-yes <anikush@hotmail.com>
2025-03-07 13:25:03 +0200

    modified 3 files
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- cypress/e2e/Tests_UI/01.Test.NavigationMenu.cy.js
+++ cypress/e2e/Tests_UI/01.Test.NavigationMenu.cy.js
@@ -45,5 +45,14 @@ cy.get('.elementor-element-2fbda9c.elementor-sticky--active > :nth-child(1) > :n
 cy.url().should('eq', 'https://kimchinamai.lt/susisiekite-su-mumis');
 cy.contains('SUSISIEKITE SU MUMIS').should('be.visible');
 
+
+cy.get('.pk-nav-link').contains('PARDUOTUVĖ')  // Locate the "PARDUOTUVĖ" link
+  .trigger('mouseover', { force: true });        // Hover over it (using force in case it’s hidden)
+
+cy.wait(500);                                    // Wait 500ms to allow the dropdown to appear
+
+cy.get('.pk-nav-li.pk-align-auto.pk-dropdown-open') // Correct selector for the dropdown element
+  .should('be.visible');
+
 });
 });
