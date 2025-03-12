4a0be637a5a1a4e66d8c4d38fbc488b0dcff939f
anikush-yes <anikush@hotmail.com>
2025-03-12 11:13:13 +0200

    modified 14.Test.SubmittinganEmptyContactForm.cy.js in Tests_UI
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- cypress/e2e/Tests_UI/14.Test.SubmittinganEmptyContactForm.cy.js
+++ cypress/e2e/Tests_UI/14.Test.SubmittinganEmptyContactForm.cy.js
@@ -4,34 +4,43 @@ describe('Submitting an Empty Contact Form', () => {
 
     it('The system should display validation errors when submitting an empty contact form.', () => {
 
-        // 14.1 Open the homepage and verify that it loads successfully.
+        // 14.1 
         cy.launchBrowser();
 
-        // 14.2 Click on the "Kontaktai" (Contact) link in the navigation menu.
+        // 14.2 
         cy.get('.elementor-element-2fbda9c.elementor-sticky--active > :nth-child(1) > :nth-child(1) > .elementor-element-326c126 > :nth-child(1) > :nth-child(1) > .elementor-element-00ee875 > :nth-child(1) > .pk-ce-widget-wrapper > .pk-ce-widget > .pk-nav > .pk-nav-ul > :nth-child(4) > .flex-container > .pk-nav-link').click({ force: true });
 
-        // 14.3 Verify that the contact page loads correctly.
+        // 14.3 
         cy.url().should('eq', 'https://kimchinamai.lt/susisiekite-su-mumis');
         cy.contains('SUSISIEKITE SU MUMIS').should('be.visible');
 
-        // 14.4 Leave all fields blank.
+        // 14.4 
         cy.get('#id-contact-184cc202')
             .select('Vartotojų pagalba')
             .should('have.value', '2');
 
         cy.get('input[name="from"]')
-            .clear(); // Leave email field empty
+            .clear(); 
 
         cy.get('textarea[name="message"]')
-            .clear(); // Leave message field empty
+            .clear(); 
 
-        // 14.5 Click submit.
+        // 14.5 
         cy.get('.elementor-field-type-submit > .elementor-button').click();
 
-        // 14.6 Verify the error message appears after submission.
-        ///Not working
-        cy.contains('Please fill out this field')
-            .should('be.visible');
+        // 14.6 
+    
+        cy.get('input:invalid, textarea:invalid').should('have.length', 3);
+
+        
+        cy.get('input[name="from"]').then(($input) => {
+          expect($input[0].validationMessage).to.eq('Please fill out this field.');
+        });
+        
+    
+        cy.get('textarea[name="message"]').then(($textarea) => {
+          expect($textarea[0].validationMessage).to.eq('Please fill out this field.');
 
     });
-})
+});
+});
