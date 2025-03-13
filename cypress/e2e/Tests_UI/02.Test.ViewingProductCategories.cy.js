7330b338811cd523fc7f25ca0c2775624a9fa35c
anikush-yes <anikush@hotmail.com>
2025-03-13 12:10:48 +0200

    modified 02.Test.ViewingProductCategories.cy.js in Tests_UI
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- cypress/e2e/Tests_UI/02.Test.ViewingProductCategories.cy.js
+++ cypress/e2e/Tests_UI/02.Test.ViewingProductCategories.cy.js
@@ -2,77 +2,53 @@
 
 describe('Product Categories Page', () => {
 
-    it('Should check each category and verify relevant products', () => {
-
-        
+    beforeEach(() => { // Fixed arrow function syntax
         cy.launchBrowser();
-    
-        
+
         cy.get('.pk-nav-link')
             .contains('PARDUOTUVĖ')
-            .should('not.have.css', 'display', 'none') 
+            .should('not.have.css', 'display', 'none')
             .trigger('mouseover', { force: true });
-           
+    });
+
+    it('Should check Kimchi category and verify relevant products', () => {
         cy.get('a[href="https://kimchinamai.lt/10-parduotuve#kimchi"]')
             .contains('KIMCHI')
             .click({ force: true });
         cy.wait(1000);
         cy.url().should('eq', 'https://kimchinamai.lt/10-parduotuve#kimchi');
-        
-        ////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Čia lūžta!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        
-        cy.get('.pk-nav-link')
-            .contains('PARDUOTUVĖ')
-            .should('not.have.css', 'display', 'none')
-            .trigger('mouseover', { force: true });
+    });
 
+    it('Should check Rinkiniai category and verify relevant products', () => {
         cy.get('a[href="https://kimchinamai.lt/10-parduotuve#rinkiniai"]')
             .contains('RINKINIAI')
             .click({ force: true });
         cy.wait(5000);
         cy.url().should('eq', 'https://kimchinamai.lt/10-parduotuve#rinkiniai');
+    });
 
-        cy.get('.pk-nav-link')
-            .contains('PARDUOTUVĖ')
-            .should('not.have.css', 'display', 'none')
-            .trigger('mouseover', { force: true });
-
-        cy.wait(4000);
+    it('Should check Sultys category and verify relevant products', () => {
         cy.get('a[href="https://kimchinamai.lt/10-parduotuve#sultys"]')
             .contains('PROBIOTINĖS SULTYS')
             .click({ force: true });
         cy.wait(4000);
         cy.url().should('eq', 'https://kimchinamai.lt/10-parduotuve#sultys');
+    });
 
-        cy.get('.pk-nav-link')
-            .contains('PARDUOTUVĖ')
-            .should('not.have.css', 'display', 'none')
-            .trigger('mouseover', { force: true });
-
+    it('Should check Padažai category and verify relevant products', () => {
         cy.get('a[href="https://kimchinamai.lt/10-parduotuve#padazai"]')
             .contains('FERMENTUOTI PADAŽAI')
             .click({ force: true });
         cy.wait(4000);
         cy.url().should('eq', 'https://kimchinamai.lt/10-parduotuve#padazai');
+    });
 
-        cy.get('.pk-nav-link')
-            .contains('PARDUOTUVĖ')
-            .should('not.have.css', 'display', 'none')
-            .trigger('mouseover', { force: true });
-
+    it('Should check Kita category and verify relevant products', () => {
         cy.get('a[href="https://kimchinamai.lt/10-parduotuve#kita"]')
             .contains('KITA')
             .click({ force: true });
         cy.wait(4000);
         cy.url().should('eq', 'https://kimchinamai.lt/10-parduotuve#kita');
-
     });
+
 });
- 
-//<a href="https://kimchinamai.lt/10-parduotuve#kimchi">KIMCHI</a>
-//<a href="https://kimchinamai.lt/10-parduotuve#rinkiniai">RINKINIAI</a>
-//<a href="https://kimchinamai.lt/10-parduotuve#sultys">PROBIOTINĖS SULTYS</a>
-//<a href="https://kimchinamai.lt/10-parduotuve#padazai">FERMENTUOTI PADAŽAI</a>
-//<a href="https://kimchinamai.lt/10-parduotuve#kita">KITA</a>
-
-
